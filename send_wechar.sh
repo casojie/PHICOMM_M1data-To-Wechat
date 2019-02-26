38e932b1a0f87884fdfbe9677ca732ad9d192221
caojie <caojiedage@outlook.com>
2019-02-26 09:28:44 +0800

    参数添加server酱key

--- send_wechar.sh
+++ send_wechar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 today=`date +"%Y-%m-%d"`
 content=`cat m1_airdata.txt | grep -v "^$"`
-curl "https://sc.ftqq.com/SCU34310T59d5701493a443f694e73757af5989f25bc72eff8281e.send?text=${today}" -d "&desp=${content}"
+curl "https://sc.ftqq.com/$1.send?text=${today}" -d "&desp=${content}"
 sed -i '3,$d' m1_airdata.txt
