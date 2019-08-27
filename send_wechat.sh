853f062064edde51e2a5140709c88464147770b1
casojie <casojie@qq.com>
2019-08-27 20:43:23 +0800

    程序基本完善

--- send_wechat.sh
+++ send_wechat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 today=`date +"%Y-%m-%d"`
 content=`cat release/m1_airdata.txt | grep -v "^$"`
-curl "https://pushbear.ftqq.com/sub?sendkey=$1&text=${today}" -d "&desp=${content}"
+curl "https://sc.ftqq.com/$1.send?text=${today}" -d "desp=${content}"
 sed -i '3,$d' release/m1_airdata.txt
